a35a79a33ac8c8177f25e47bb2e44c32da0b7927
SkyChen1009 <skychen901009@gmail.com>
2024-10-28 00:46:20 +0800

    done

---
+++ helpers/data_mining_helpers.py
@@ -0,0 +1,32 @@
+import nltk
+
+def format_rows(docs):
+    """ format the text field and strip special characters """
+    D = []
+    for d in docs:  # Directly iterate over the Series
+        temp_d = " ".join(d.split("\n")).strip('\n\t')
+        D.append([temp_d])
+    return D
+
+def format_labels(target, docs):
+    """ format the labels """
+    return docs.target_names[target]
+
+def check_missing_values(row):
+    """ functions that check and verifies if there are missing values in dataframe """
+    counter = 0
+    for element in row:
+        if element == True:
+            counter += 1
+    return ("The amount of missing records is: ", counter)
+
+def tokenize_text(text, remove_stopwords=False):
+    """
+    Tokenize text using the nltk library
+    """
+    tokens = []
+    for d in nltk.sent_tokenize(text, language='english'):
+        for word in nltk.word_tokenize(d, language='english'):
+            # filters here
+            tokens.append(word)
+    return tokens
